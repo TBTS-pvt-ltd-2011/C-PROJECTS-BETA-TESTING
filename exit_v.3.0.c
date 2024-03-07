3ab39fce0206b6a44a777dbf52f5b143c1be0c6a
The Beta Testing Studios <ps1894002@gmail.com>
2024-03-07 15:14:37 +0530

    Update exit_driver_03.c

--- exit_v.3.0.c
+++ exit_v.3.0.c
@@ -1,16 +1,14 @@
 
-// C++ program to demonstrate Segmentation Fault
 #include <iostream>
 using namespace std;
  
 // Driver Code
 int main()
 {
-    // An array of size 100
+    // An array of size 100 is used
     int arr[100] = { 0 };
  
-    // When we try to access the array out
-    // of bound, it will give Segmentation Fault
+    // Note: When we try to access the array out of bound, it will give Segmentation Fault
     cout << arr[100001];
     return 0;
 }
