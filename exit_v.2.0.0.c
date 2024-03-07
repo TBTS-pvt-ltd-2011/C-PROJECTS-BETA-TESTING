bd244565a412bc4050ce77e4db40fefa46b4b9ec
The Beta Testing Studios <ps1894002@gmail.com>
2024-03-07 15:13:46 +0530

    Update exit_driver_02.c

--- exit_v.2.0.0.c
+++ exit_v.2.0.0.c
@@ -1,16 +1,13 @@
 
-// C++ program to demonstrate
-// Floating Point Error
 #include <iostream>
 using namespace std;
  
-// Driver Code
+// The code for the driver
 int main()
 {
     int a = 1, b = 0;
  
-    // When we try to divide by zero
-    // it should give SIGFPE
+    // Note: When we try to divide by zero it should give SIGFPE
     cout << a / b;
     return 0;
 }
