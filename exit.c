2b21a28924b7c7c1089704ec229fdb51c3254e6d
The Beta Testing Studios <ps1894002@gmail.com>
2024-03-07 15:04:38 +0530

    Update exit_driver.c

--- exit.c
+++ exit.c
@@ -1,14 +1,11 @@
 
-// C++ program to demonstrate TLE
 #include <iostream>
 using namespace std;
 
-// Driver Code
+// The code for the driver
 int main()
 {
 	// Below statement will give time
-	// limit exceeded as well as memory
-	// limit exceeded due to infinite loop
 	for (;;) {
 		int arr[10000];
 	}
